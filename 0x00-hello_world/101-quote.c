563b75af5ebdb04676f98953ea70e521e54828af
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-06-15 14:32:23 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\"
-Dora Korpar, 2015-10-19\n";
-write(2, message, 59);
+char *mesage = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, mesage, 59);
 return (1);
 }
