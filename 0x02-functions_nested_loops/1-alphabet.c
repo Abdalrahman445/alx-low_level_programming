2ea776b84b69811f534db73ae030e28bfe315afe
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-06-21 17:29:10 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * a fuction to print the alphapet 
+ * print_alphabet - is a fuction to print the alphabet
+ * letters in lowercase
  */
 void print_alphabet(void)
 {
 int i;
 
-for(i = 'a' ; i <= 'z' ; i++)
+for (i = 'a' ; i <= 'z' ; i++)
 {
 _putchar(i);
 }
