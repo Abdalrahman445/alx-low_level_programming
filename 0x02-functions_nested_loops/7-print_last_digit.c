43382397a6e60c624d91a3df0d4861a969a39fda
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-06-22 05:43:19 -0700

    c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,6 @@
 int print_last_digit(int n)
 {
 int last_digit;
-int positive;
 
 if (n >= 0)
 {
@@ -17,8 +16,8 @@ last_digit = n % 10;
 }
 else
 {
-positive = n * -1;
-last_digit = positive % 10;
+n = n * -1;
+last_digit = n % 10;
 }
 _putchar(last_digit + '0');
 return (last_digit);
