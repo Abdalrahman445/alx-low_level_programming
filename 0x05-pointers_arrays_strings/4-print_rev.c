4f7c9808903be83ea04bfcb9c397fc4c23c47ca4
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-06-26 09:06:04 -0700

    c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * print_rev - a function that prints a string,
+ * in reverse, followed by a new line.
  * @s: the string we want to print it in reverse
  */
 void print_rev(char *s)
 {
-int i; 
+int i;
 int j;
 
 i = strlen(s);
