94a578e3bbf54e69c00a68addb179e68cda41f94
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-07-03 04:38:03 -0700

    c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,6 +6,7 @@
  * @n: the number of bytes we want to change
  * @b: the value we want to assign
  * @s: the memory area we work on
+ * Return: the memory set we want
  */
 char *_memset(char *s, char b, unsigned int n)
 {
