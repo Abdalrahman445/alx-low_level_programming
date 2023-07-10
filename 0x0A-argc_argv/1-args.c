758ae427f35288fd24d80ed5ee150d7c0435cb00
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-07-10 09:26:00 -0700

    c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,5 +3,6 @@
 
 void _putchar(char c);
 int main(int __attribute__((unused)) argc ,char *argv[]);
+int main(int argc, char __attribute__((unused)) *argv[]);
 
 #endif
