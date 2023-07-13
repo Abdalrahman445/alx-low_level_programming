8fefa4829f42c728cb0a8c786152783ef093f4c1
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-07-12 18:16:20 -0700

    c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,8 +6,6 @@
  * @s1: the string we want to concatenates
  * @s2: the other string we want to concatenates
  * Return:pointer should point to a newly allocated space in memory
- * which contains the contents of s1,
- * followed by the contents of s2, and null terminated
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -15,7 +13,8 @@ int size1;
 int size2;
 char *a;
 
-size1 = 0; size2 = 0;
+size1 = 0;
+size2 = 0;
 if (s1 == NULL)
 {
 s1 = "";
@@ -37,7 +36,8 @@ if (a == NULL)
 {
 return (NULL);
 }
-size1 = 0; size2 = 0;
+size1 = 0;
+size2 = 0;
 while (s1[size1] != '\0')
 {
 a[size1] = s1[size1];
@@ -46,7 +46,7 @@ size1++;
 while (s2[size2] != '\0')
 {
 a[size1] = s2[size2];
-size1++; size2++;
+size1++, size2++;
 }
 a[size1] = '\0';
 return (a);
