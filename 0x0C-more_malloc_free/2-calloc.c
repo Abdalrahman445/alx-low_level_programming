c3fb8592e2295b2b7901b62e14a4805887c77c7b
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-07-14 09:47:16 -0700

    c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ if (nmemb == 0 || size == 0)
 i = malloc(nmemb * size);
 if (i == NULL)
 	return (NULL);
-for (j = 0 ; j < nmemb ; j++)
+for (j = 0 ; j < (nmemb * size) ; j++)
        i[j] = 0;
 return (i);
 }
