cc49732cbee5b4afb6225a5b8987124fee3d294c
Abdalrahman445 <abdalrahmanahmedd2003@gmail.com>
2023-07-18 14:25:54 -0700

    c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,11 +11,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *my_dog;
+
+int len1;
+int len2;
+
+len1 = 0;
+len2 = 0;
+while (name[len1] != '\0')
+	len1++;
+while (owner[len2] != '\0')
+	len2++;
 my_dog = malloc(sizeof(dog_t));
 if (my_dog == NULL)
 	return (NULL);
-my_dog->name = name;
+my_dog->name = malloc(sizeof(char) * (len1 + 1));
+if (my_dog->name == NULL)
+	return (NULL);
+my_dog->owner = malloc(sizeof(char) * (len2 + 1));
+if (my_dog->owner == NULL)
+	return (NULL);
+_strcpy(my_dog->name, name);
+_strcpy(my_dog->owner, owner);
 my_dog->age = age;
-my_dog->owner = owner;
 return (my_dog);
+}
+/**
+ * _strcpy - a function to copy strings
+ * @dest:the location we want to paste in
+ * @src: the string we want to copy
+ * Return: a pointer of dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len;
+int i;
+
+len = 0;
+while (src[len] != '\0')
+	len++;
+for (i = 0 ; i < len ; i++)
+	dest[i] = src[i];
+dest[i] = '\0';
+return (dest);
 }
